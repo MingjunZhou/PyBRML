3617e6434315f4afb8b93d374c172b05dbfa2a03
Mingjun Zhou <mingjun.zhou@gmail.com>
2013-08-16 03:22:06 +0800

    add demoMRFclean.py

---
+++ demo/demoMRFclean.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+# Python implementation of BRMLtoolbox
+# Author: Mingjun Zhou
+# License: GNU License
+
+"""
+DEMOMRFCLEAN demo of image denoising using a binary state Markov Random Field
+"""
+
+print __doc__
+
+import numpy as np
+import sys
+sys.path.append("..")
+from brml.potential import potential
