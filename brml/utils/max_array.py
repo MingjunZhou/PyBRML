94eaab721d2e3a03115a66494d2ac31654097500
Mingjun Zhou <mingjun.zhou@gmail.com>
2013-11-07 18:00:05 +0800

    DONE: max_array.py

--- brml/utils/max_array.py
+++ brml/utils/max_array.py
@@ -6,7 +6,6 @@
 #Description :
 
 import numpy as np
-from .set_diff import set_diff
 from .index_to_assignment import index_to_assignment
 from .assignment_to_index import assignment_to_index
 
@@ -15,7 +14,8 @@ def max_array(x, max_over, return_states=False):
     """Maximise a multi-dimensional array over a set of dimensions.
 
     Usage :
-        max_val, max_state = max_array(x, max_over)
+        max_val = max_array(x, max_over)
+        max_val, max_state = max_array(x, max_over, return_states=True)
 
     Parameters :
         x : nd.array[n_dim1, n_dim2, ...] :
@@ -34,44 +34,57 @@ def max_array(x, max_over, return_states=False):
         max_val : nd.array[n_remaining_dim1, ...] :
             The remaining array.
 
-        max_state : nd.array[n_over, ] :
-            When the variables in max_over equal to the states in max_state,
-            we get the maximised array.
+        max_state : nd.array[n_left_dims, n_dims_of_x] :
+            The row vetcor in max_state[i, :] is the assignments in x. That
+            is, x[max_state[i, :]] == max_val[i].
 
     Examples :
         max_val, max_state = max_array(np.rand(3,4,2), [3, 1])
 
         We maximise over the 3rd and 1st dimensions, of the 3x4x2 table. This
         means that after maximising, the remaining array is a 4x1 array.
-        max_val(i) contains the maximum value of the array when the second
-        dimension is in state i.
+        x[max_state[i, :]] == max_val[i]
 
     Raises :
         None
 
     """
+    if not isinstance(max_over, np.ndarray):
+        max_over = np.array(max_over)
+
     if return_states is False:
         # works faster if just need max value
         xx = np.array(x)
-        for dim in max_over:
+        for dim in np.sort(max_over)[::-1]:
             xx = np.max(xx, axis=dim)
         max_val = np.squeeze(xx)
         return max_val
     else:
-        if not isinstance(max_over, np.ndarray):
-            max_over = np.array(max_over)
-        shape = x.shape
+        shape = np.array(x.shape)
         n_dims = x.ndim
         dims = np.arange(n_dims)
-        left = dims.differences(max_over)
+        left = np.setdiff1d(dims, max_over)
         dim_left = np.prod(shape[left])
         dim_over = np.prod(shape[max_over])
-        xx = np.transpose(x, [left, max_over])
+        concat_left_over = np.concatenate((left, max_over))
+        xx = np.transpose(x, concat_left_over)
         s = np.reshape(xx, [dim_left, dim_over])
         max_val, idx = s.max(1), s.argmax(1)
-        c = np.concatenate(np.arange(dim_left).transpose(), idx.transpose())
-        assignment = assignment_to_index(c, [dim_left, dim_over])
-        concat_left_over = np.concatenate((left, max_over))
-        max_state = index_to_assignment(assignment, dims[concat_left_over])
-        p, q = concat_left_over.sort(), concat_left_over.argsort()
+
+        ranges_left = np.arange(dim_left).reshape(dim_left, -1)
+        c = np.concatenate((ranges_left, idx.reshape(dim_left, -1)), axis=1)
+
+        idxs_all = []
+        for assignment in c:
+            idxs_all.append(assignment_to_index(assignment,
+                                                [dim_left, dim_over]))
+
+        ass_all = []
+        for idx in idxs_all:
+            ass_all.append(index_to_assignment(idx, shape[concat_left_over]))
+        max_state = np.array(ass_all)
+        q = concat_left_over.argsort()
+
+        if len(left) > 1:
+            max_val = max_val.reshape(shape[left])
         return max_val, max_state[:, q]
